b45beb88b65b30b8775095dc2602662eaaadd2b9
jiruneko <oohikaru310@gmail.com>
2025-06-06 08:20:10 +0900

    secound commit

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { Item } from './items.model';
 
 @Injectable()
 export class ItemsService {
-  findAll() {
-    return 'This is ItemsService';
+  private items: Item[] = [];
+
+  findAll(): Item[] {
+    return this.items;
+  }
+
+  findById(id: string): Item {
+    const items = this.items.find((item) => item.id === id);
+    if (!items) {
+      throw new Error('商品が存在しません');
+    }
+    return items;
+  }
+
+  create(item: Item): Item {
+    this.items.push(item);
+    return item;
   }
 }
